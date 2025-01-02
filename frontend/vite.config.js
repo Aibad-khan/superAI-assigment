a7d9890a064b18c6ea8a4ab834d5a8ba9946ded8
Aibad <143527312+Aibad-khan@users.noreply.github.com>
2025-01-02 11:52:04 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // Backend server URL
+        target: 'https://superai-assigment.onrender.com', // Backend server URL
         changeOrigin: true,
         
       },
